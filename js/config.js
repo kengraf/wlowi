d90d6b476085c3f5793d39bb569ab580db5026df
kengraf <ken@securityxing.com>
2021-02-02 23:37:42 -0500

    cognito config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_D4v8EeMwW',
+        userPoolClientId: '1mhvueq5mtjjtk3v3rp4d4fjgv', 
+        region: 'us-east-2'
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
