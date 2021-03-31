919b9e7c15178a66976e3262c943b4514bfc9602
kengraf <ken@securityxing.com>
2021-03-30 23:21:22 -0400

    treasure hunt

---
+++ treasure-hunt-nh.js
@@ -0,0 +1,197 @@
+var hunt = [
+    {
+        "id": "1",
+        "title": "Rocket",
+        "story": {
+            "image": "images/rocket.png",
+            "link": "https://www.atlasobscura.com/places/redstone-rocket",
+            "imageCaption": "Sitting on the village green in the town of Warren, NH, is a Redstone ballistic missle",
+            "url": "http://www.cowhampshireblog.com/2006/11/24/the-day-warren-new-hampshire-went-ballistic/"
+            },
+        "position": { "lat": 43.92521, "lng": -71.89156 },
+        "link": "",
+        "riddle": "These pairs have something in common.  One is the only in the world, the last has a rocket, what is the first?<div><p class='text-center'>deer, brook <br> dia, aan <br> niker, gos <br>ren, ner <br>  ber, dub <br> and, han <br> bon, hol</p></div>",
+        "answer": "andover",
+        "md5": "bcc3c4fc8319820c34c239158f41ea03"
+
+    },
+    {
+        "id": "2",
+        "title": "Susannah Willard Johnson",
+        "position": { "lat": 43.25553, "lng": -72.43126 },
+        "story": {
+            "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Fort_at_Number_4%2C_view%2C_Charlestown_NH.jpg/320px-Fort_at_Number_4%2C_view%2C_Charlestown_NH.jpg",
+            "link": "https://upload.wikimedia.org",
+            "imageCaption": "Fort #4",
+            "url": "https://en.wikipedia.org/wiki/Susannah_Willard_Johnson"
+            },
+        "riddle": "She said: 'But in an instant a crowd of savages, fixed horribly for war, rushed furiously in. I screamed and begged my friends to ask for quarter'.  The name of her eldest son is the XOR key you need.<div><p class='text-center'>MhsJGAAFHHk=</p></div>",
+        "answer": "Abenaki",
+        "md5": "5584445c6425888fc463902aa40d0ec5"
+
+    },
+    {
+        "id": "3",
+        "title": "Frost",
+        "story": {
+            "image": "images/frost.png",
+            "link": "https://www.nhstateparks.org/visit/historic-sites/robert-frost-farm-historic-site",
+            "imageCaption": "Robert Frost's simple two-story white clapboard farmhouse is typical of New England in the 1880s",
+            "url": "https://www.poetryfoundation.org/poets/robert-frost"
+            },
+        "position": { "lat": 42.87173, "lng": -71.29488 },
+         "riddle": "<div>3882 3405 5375 3882 5952</div>In a one-part code, the code groups are assigned to the letters, words or phrases that they represent in a logical order - typically, alphabetical order. <br>The above message - which you may find familiar - has been encrypted using a one-part code, in which the code groups 0000 to 9999 represent 10,000 of the commonest English words in alphabetically order<br><a href='https://github.com/first20hours/google-10000-english/blob/master/google-10000-english.txt'>Google 10000 english words</a>",
+        "answer": "good fences make good neighbors",
+        "md5": "313c5a0c4f51067d79d19f61ebf0a2bc"
+
+    },
+    {
+        "id": "4",
+        "title": "Passaconaway",
+        "story": {
+            "image": "https://www.nhhistory.org/CMSPages/GetFile.aspx?guid=cf8771bd-90aa-4164-8725-51627053559a&maxsidesize=230",
+            "link": "https://www.nhhistory.org/Timeline?id=1676.1",
+            "imageCaption": "Sham Battle",
+            "url": "http://www.seacoastnh.com/history/as-i-please/tracking-passaconaway/"
+            },
+        "position": { "lat": 44.05080, "lng": -71.56672 },
+         "riddle": "Rail fences are great for managing livestock, not so good at preventing attacks. Who is the poem about?  FYI: Almost all of the rail fences in the south were consumed by the Confederate army for firewood during the Civil War. Who is the message about?<div><p class='text-center'>Wom  &nbsp&nbspo smiagW &nbspoidthedro<br>tmihh .a faattdfnPokeI a e nhd i horncfrSrn tateinn eehu;A henclc &nbspanfrrg cgAhloiec<br>hnncstns &nbspnv ineao g<br>&nbspuhac irdApddttwnnel n<br>avedeoasnKnu<br>eor na Tdahm.Autbee<br>c dmude.Ema kdrf<br>&nbspln i adi oCu Eoc erwoas tn ie<br>lenh ltdde &nbspuwl so ib&nbsp<br></p></div>",
+        "answer": "kancamagus",
+        "md5": "535d9851c65660ce73ccc26eec603d6d"
+
+    },
+    {
+        "id": "5",
+        "title": "General Stark",
+        "story": {
+            "image": "https://upload.wikimedia.org/wikipedia/commons/2/29/General_John_Stark_Edit.jpg",
+            "link": "https://en.wikipedia.org/wiki/John_Stark",
+            "imageCaption": "Image of John Stark",
+            "url": "https://www.battlefields.org/learn/biographies/john-stark"
+            },
+        "story1": "w<b>h</b>i<b>l</b>e o<b>n</b> a hu<b>n</b>ti<b>ng</b> ",
+        "position": { "lat": 43.01458, "lng": -71.47075 },
+        "riddle": "wHiLe oN a huNtiNG anD trapPiNG trip alOng thE bakER rIVer, a tribUTaRy of thE pemigEWasSet river, He wAs caPTUrEd by AbenAkI WarrIOrS And BrOugHt baCH To cAnaDa But nOT bEFore wArniNG hiS brOThEr wIlLiaM to PadDLe Away iN hIs CanoE",
+         "answer": "Live free or die: Death is not the worst of evils",
+        "md5": "ae1419049c12f0ea48902d20e7317459"
+
+    },
+    {
+        "id": "6",
+        "title": "General Sullivan",
+        "story": {
+            "image": "https://upload.wikimedia.org/wikipedia/commons/7/7b/GeneralJohnSullivanByTenney.jpg",
+            "link": "https://en.wikipedia.org/wiki/John_Sullivan_(general)",
+            "imageCaption": "John Sullivan (February 17, 1740 – January 23, 1795) was an Irish-American General",
+            "url": "https://en.wikipedia.org/wiki/John_Sullivan_(general)"
+            },
+        "position": { "lat": 43.13048, "lng": -70.91770 },
+        "riddle": "Which location is most northerly?<div><p class='text-center'>746P+X8 Born<br>37CQ+HX Raided<br>M2G6+2R Captured<br>66CJ+5P Rejoined Washington<br>27W8+4M Expedition<br>43JJ+4W Died</p></div>",
+        "answer": "746P+X8",
+        "md5": "ec53e2155843b13d25650765c020fad3 "
+
+    },
+    {
+        "id": "7",
+        "title": "Lighter than air",
+        "story": {
+            "image": "https://upload.wikimedia.org/wikipedia/commons/7/71/Balloon_Corps.jpg",
+            "link": "https://en.wikipedia.org/wiki/Union_Army_Balloon_Corps",
+            "imageCaption": "Woodblock sketch of Lowe's balloon ",
+            "url": "https://en.wikipedia.org/wiki/Thaddeus_S._C._Lowe"
+            },
+        "position": { "lat": 44.41941, "lng": -71.47489 },
+        "riddle": "Some might have called him: <div><p class='text-center'>66.7.95.53.6 <br>95.68.53.20.7 <br>74.18 <br>75.34.18.6.1 <br>32.28.92.16</p></div>",
+        "answer": "DyNAmIC AmErICaN WAr ReSeArCH GeNiUS",
+        "md5": "64f6f7f978163b5183aa0c9ff79a31b5"
+
+    },
+    {
+        "id": "8",
+        "title": "Black Heritage Trail",
+        "story": {
+            "image": "images/blackheritage.png",
+            "link": "https://www.tripadvisor.com/Attraction_Review-g46209-d5961990-Reviews-Portsmouth_Black_Heritage_Trail-Portsmouth_New_Hampshire.html",
+            "imageCaption": "Trip Advisor pictures",
+            "url": "https://www.nhbr.com/black-heritage-trail-uncovers-hidden-history/"
+            },
+        "position": { "lat": 43.07596, "lng": -70.75732 },
+        "riddle": "Follow the Portsmouth Black Heritage Trail.  Which location does not have a trail marker?<div><p class='text-center'>ducks.stand.drag<br>excuse.factor.chose<br>space.beans.fault<br>truly.honest.onion<br>hello.forced.hoot<br>torch.dusty.precautions<br>spell.puzzle.weds<br>slide.sulk.others<br>action.rich.robot</p></div>",
+        "answer": "action.rich.robot",
+        "md5": "f7f464df7777cbf6285ff96608e26f89"
+
+    },
+];
+
+function validAnswer(id) {
+  var theGuess = document.getElementById('answer'+id).value;
+  var theAnswer = hunt[id-1].md5;
+  var color = "bg-danger";
+
+  theGuess = theGuess.replace(/\W/g,"").toLowerCase();
+  var passhash = CryptoJS.MD5(theGuess).toString();
+    if( passhash === theAnswer ) {
+        color = "bg-success";
+    }
+    
+    var clist = document.getElementById("card"+id).classList;
+    if( clist.contains('bg-primary') ) { clist.remove("bg-primary"); }
+    if( clist.contains('bg-danger') ) { clist.remove("bg-danger"); }
+    if( clist.contains('bg-sucess') ) { clist.remove("bg-success"); }
+    clist.add( color );
+}
+
+function createRiddleCard(riddle) {
+    const args = riddle.id + riddle.md5;
+    var html = [
+        '<div class="card accordion-item" id="card' + riddle.id + '"><h2 class="accordion-header">',
+        '<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse' + riddle.id + '" ',
+        'aria-expanded="true" aria-controls="collapse' + riddle.id + '">' + riddle.title + '</button></h2>',
+        '<div id="collapse' + riddle.id + '" class="accordion-collapse collapse"',
+        'aria-labelledby="card' + riddle.id + '" data-bs-parent="#accordionDiv">',
+        '<div class="accordion-body">' + riddle.riddle,
+        '<input type="text" placeholder="Answer: ' + riddle.answer + '" id="answer' + riddle.id + '">',
+        '<button id="button' + riddle.id + '" onclick="validAnswer(' + riddle.id + ')" ',
+        'class="btn btn-primary">Submit</button></div></div></div>',
+    ].join('\n');
+    var div = document.getElementById('accordionDiv')
+    div.innerHTML += html;
+}
+
+// This displays various markers.
+// When the user clicks a marker, an info window opens.
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 8,
+    center: {lat: 43.57219, lng: -71.49520},
+  });
+
+  var infowindow = new google.maps.InfoWindow();
+
+  hunt.forEach(function(h) {
+    var content = '<div id="content"><div id="siteNotice"></div>' +
+          '<h2 id="firstHeading" class="firstHeading">' + h.title + '</h2><div id="bodyContent">' +
+          '<a title="View source of image" href="' + h.story.link + '"target="_blank">' +
+             '<img title="' + h.story.imageCaption + '" src="' + h.story.image + '" style="height:200px;"></a>';
+    if( h.story.url.length != 0 ) {
+        content += '<div><a href="' + h.story.url + '" target="_blank"><h5>More info</h5></a></div>'
+        }
+    content += '</div></div>';
+    var marker = new google.maps.Marker({
+      position: h.position,
+      map: map,
+      title: h.title,
+    });
+    google.maps.event.addListener(marker,'click', (function(marker,content,infowindow) { 
+        return function() {
+            infowindow.setContent(content);
+            infowindow.open(map,marker);
+        };
+    })(marker,content,infowindow));
+        });
+}  
+
+
+hunt.forEach(function(h) {
+    createRiddleCard(h);
+});
